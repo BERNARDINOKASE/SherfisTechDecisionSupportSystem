98103c15f4d5048c1bd94a083932d12089c0f674
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-05-03 03:41:30 +0800

    fix: fix update vektor s for alternatif

--- app/Http/Controllers/WPController.php
+++ app/Http/Controllers/WPController.php
@@ -6,6 +6,8 @@ use App\Models\Alternatif;
 use App\Models\Kriteria;
 use App\Models\Nilai;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Psy\VersionUpdater\SelfUpdate;
 
 class WPController extends Controller
 {
@@ -14,41 +16,15 @@ class WPController extends Controller
         $kriteria = Kriteria::select('id_kriteria', 'nama_kriteria')->orderBy('id_kriteria', 'asc')->get();
         $alternatif = Alternatif::select('id_alternatif', 'nama_alternatif')->get();
         $nilai = Nilai::select('kondisi', 'id_alternatif', 'id_kriteria')->get();
+        // $hasil_vektor_s_all = $this->vektor_s();
+
         // dd($alternatif('id_alternatif'));
         // $nilai = Nilai::where('id_alternatif', $alternatif)->select('id_alternatif', 'id_kriteria', 'kondisi')->get();
-        return view('content.metode.index', compact('kriteria', 'alternatif', 'nilai'));
+        return view('content.metode.index', compact('kriteria', 'alternatif', 'nilai', 'hasil_vektor_s_all'));
     }
 
     public function vektor_s()
     {
-        // $alternatif = Alternatif::pluck('id_alternatif')->toArray();
-        // $data = Kriteria::orderBy('created_at', 'ASC')->get();
-        // $sum_kriteria = Kriteria::sum('bobot_kriteria');
-
-        // $normalisasi = [];
-        // $id_kriteria = Kriteria::pluck('id_kriteria');
-        // $kondisi = Nilai::whereIn('id_alternatif', $alternatif)->whereIn('id_kriteria', $id_kriteria)->orderBy('created_at')->pluck('kondisi')->toArray();
-
-        // foreach ($data as $item) {
-        //     $hasil = $item->bobot_kriteria / $sum_kriteria; // Hitung normalisasi bobot kriteria
-        //     $normalisasi[] = $hasil;
-        // }
-
-        // $vektor_s = []; // Inisialisasi array untuk nilai vektor S
-
-        // // Perulangan untuk menghitung nilai vektor S
-        // foreach ($kondisi as $key => $item) {
-        //     if (isset($normalisasi[$key])) {
-        //         $vektor_s[$key] = pow($item, $normalisasi[$key]); // Hitung nilai vektor S
-        //     }
-        // }
-        // $hasil_vektor_s = array_product($vektor_s);
-        // dd($hasil_vektor_s);
-        // // Output nilai vektor S
-        // foreach ($vektor_s as $key => $value) {
-        //     dd('Nilai Vektor S untuk Kriteria' . $id_kriteria[$key] . 'adalah : ' . $value);
-        // }
-
         $alternatif = Alternatif::pluck('id_alternatif')->toArray();
         $data = Kriteria::orderBy('created_at', 'ASC')->get();
         $sum_kriteria = Kriteria::sum('bobot_kriteria');
@@ -75,10 +51,13 @@ class WPController extends Controller
             }
 
             $hasil_vektor_s = array_product($vektor_s);
-            $hasil_vektor_s_all[] = $hasil_vektor_s;
+            // $hasil_vektor_s_all[] = $hasil_vektor_s;
             // Alternatif::where('id_alternatif', $alt)->update(['vektor_s' => $hasil_vektor_s_all]);
+            DB::table('alternatifs')->where('id_alternatif', $alt)->update(['vektor_s' => $hasil_vektor_s]);
         }
 
-        dd($hasil_vektor_s_all);
+        // return $hasil_vektor_s_all;
+        // dd($hasil_vektor_s_all);
+        // return view('content.metode.index', compact('hasil_vektor_s_all'));
     }
 }
