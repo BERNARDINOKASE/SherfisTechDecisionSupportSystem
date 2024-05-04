3b320c3ac3e6b491fe9b59458b782bd6b5e4bf4b
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-05-04 12:05:36 +0800

    style: fix layouting

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        {{-- <img
-            src="{{asset('assets/')}}/dist/img/AdminLTELogo.png"
-            alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3"
-            style="opacity: 0.8"
-        /> --}}
-        <span class="brand-text font-weight-light">SPK - Sherfis Tech</span>
+        <span class="brand-text d-flex">Sistem Pendukung<br>Keputusan</span>
     </a>
 
     <!-- Sidebar -->
